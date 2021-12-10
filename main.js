4468057f326698bb9ab548a30ec56db8184c801b
Julie Assur <78449568+limzkil@users.noreply.github.com>
2021-12-10 13:48:45 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+
+Follow these workshop creation steps:
+
+1. Rename this file name to match your workshop
+2. Add boiler plate code for the students if needed
+3. Add comments outlining the steps students should follow
+4. Provide some empty function or variable names to avoid the "blank page"
+
+*/
+
+function main() {
+  console.log('Running the "main" function!');
+}
+
+main();
