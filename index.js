ce9e986a0f7861faac26f1b4cbe315d3dea61545
[Benjamin Villa] <[benjamin.j.villa@gmail.com]>
2022-02-14 10:07:39 -0500

    first submission of Guess Number project

--- index.js
+++ index.js
@@ -12,7 +12,27 @@ start();
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
+  console.log('You entered: ' + secretNumber); 
+
+  
+    console.log("Now let's play!")
+
+    let min = 1
+    let max = 100
+    let num = Math.floor((min + max) / 2)
+
+  
+    let input = await ask("Is it... " + num + "? Y or N "); {
+    while (input !== `Y`); 
+      
+    // had a bunch of code here that was good ideas but not helping
+    // deleted most of it and made more progress than I did all weekend
+      
+    }
+  
+
+
+
   // Now try and complete the program.
   process.exit();
 }
